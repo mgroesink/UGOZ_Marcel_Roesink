299d544beaa070295d6460c7e4d9440e94e64ec7
Marcel Roesink <RSK01@rocvantwente.nl>
2021-09-24 11:37:10 +0200

    Page 21 - logo

---
+++ UGOZ_Marcel_Roesink/Models/LoginViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UGOZ_Marcel_Roesink.Models
+{
+    public class LoginViewModel
+    {
+        private string _email;
+
+        [Required(ErrorMessage = "{0} is een verplicht veld")]
+        [EmailAddress(ErrorMessage ="Dit is geen geldig e-mailadres")]
+        [Display(Name ="E-mail")]
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                //if(!value.EndsWith("rocvantwente.nl"))
+                //{
+                //    throw new Exception("Alleen ROC accounts");
+                //}
+                _email = value.ToLower();
+            }
+
+        }
+
+        // Auto property
+        [Required(ErrorMessage ="{0) is een verplicht veld") ]
+        [DataType(DataType.Password)]
+        [Display(Name ="Wachtwoord")]
+        public string Password { get; set; }
+        [Display(Name ="Onthoud mij")]
+        public bool RememberMe { get; set; }
+
+    }
+}
