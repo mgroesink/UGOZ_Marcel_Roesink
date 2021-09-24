8d3610d27f07b568e1e04784b07f4dcf9ac67ee7
Marcel Roesink <RSK01@rocvantwente.nl>
2021-09-24 09:57:29 +0200

    End of lesson 2

---
+++ UGOZ_Marcel_Roesink/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UGOZ_Marcel_Roesink.Models
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
