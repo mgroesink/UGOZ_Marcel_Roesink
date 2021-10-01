5a62b79cdb19dab0121fd9cf2cfe57956665a8a5
Marcel Roesink <RSK01@rocvantwente.nl>
2021-10-01 12:06:49 +0200

    Error fixed in login view. Tutorial: end of page 23

--- UGOZ_Marcel_Roesink/Models/ViewModels/LoginViewModel.cs
+++ UGOZ_Marcel_Roesink/Models/ViewModels/LoginViewModel.cs
@@ -8,31 +8,23 @@ namespace UGOZ_Marcel_Roesink.Models.ViewModels
 {
     public class LoginViewModel
     {
-        private string _email;
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        [EmailAddress(ErrorMessage = "Dit is geen geldig e-mailadres")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
 
-        [Required(ErrorMessage = "{0} is een verplicht veld")]
-        [EmailAddress(ErrorMessage ="Dit is geen geldig e-mailadres")]
-        [Display(Name ="E-mail")]
-        public string Email
-        {
-            get
-            {
-                return _email;
-            }
-            set
-            {
-                _email = value.ToLower();
-            }
-
-        }
-
-        // Auto property
-        [Required(ErrorMessage ="{0) is een verplicht veld") ]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
         [DataType(DataType.Password)]
-        [Display(Name ="Wachtwoord")]
+        [Display(Name = "Wachtwoord")]
         public string Password { get; set; }
-        [Display(Name ="Onthoud mij")]
+
+        [Display(Name = "Onthoud mij")]
         public bool RememberMe { get; set; }
 
     }
 }
+
+
+
+
+
