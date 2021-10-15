b78124364d374a630e18a65dfb94b155d6e5dfb5
Marcel Roesink <RSK01@rocvantwente.nl>
2021-10-15 10:42:27 +0200

    Register Action added

--- UGOZ_Marcel_Roesink/Controllers/AccountController.cs
+++ UGOZ_Marcel_Roesink/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UGOZ_Marcel_Roesink.Models;
+using UGOZ_Marcel_Roesink.Models.ViewModels;
 using UGOZ_Marcel_Roesink.Utility;
 
 namespace UGOZ_Marcel_Roesink.Controllers
@@ -11,10 +13,20 @@ namespace UGOZ_Marcel_Roesink.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ApplicationDbContext db)
+        public AccountController(ApplicationDbContext db ,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
         {
             _db = db;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _userManager = userManager;
+
         }
 
         public IActionResult Login()
@@ -31,6 +43,39 @@ namespace UGOZ_Marcel_Roesink.Controllers
 
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            // Check of model valide is
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                // Add all errors to the modelstate
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+
+            }
             return View();
         }
     }
--- UGOZ_Marcel_Roesink/Startup.cs
+++ UGOZ_Marcel_Roesink/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,8 @@ namespace UGOZ_Marcel_Roesink
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
